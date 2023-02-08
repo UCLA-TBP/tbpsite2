97b25c26183d98049b3928a7a66a705d084128ad
Sophia Shen <sophiashen@Sophias-MacBook-Pro-4.local>
2023-02-07 23:31:16 -0800

    merge api

---
+++ client/src/events/calendar.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import {google} from 'googleapis'; // Import the Google API Client Library for JavaScript
+
+function Calendar() {
+    const [events, setEvent] = useState(null); // Declare state variables to store the calendar data
+
+    useEffect(() => {
+        google.load('client', () => {
+            google.client.init({
+                apiKey: 'AIzaSyAN1BUFhyJ5x3rqxsi5WfiarEWHT3JbvB4',
+                discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'] // Specify the API discovery document for the Google Calendar API
+            }).then(() => {
+                google.client.calendar.events.list({
+                    calendarId: 'lv5b95c37926e08bbs8jj20n38@group.calendar.google.com',
+                    timeMin: new Date('2023-02-01T12:00:00').toISOString(),
+                    singleEvents: true,
+                    orderBy: 'startTime'
+                }).then((response) => {
+                    setEvent(response.result.items);
+                });
+            });
+        });
+    }, []);
+
+    return (
+        <div>
+            {events.map((event) => (
+                <div key={event.id}>
+                    <h3>{event.summary}</h3>
+                    <p>Start: {event.start.dateTime}</p>
+                    <p>End: {event.end.dateTime}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+  
+export default Calendar;
---
+++ client/src/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    fallback: {
+      "https": require.resolve("https-browserify"),
+      "fs": false,
+      "child_process": false,
+      "crypto": require.resolve("crypto-browserify"),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
