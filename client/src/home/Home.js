de292dbc1cbaf2f2e090cba8ac4e9df2cf0e3609
Sophia Shen <sophiashen@Sophias-MacBook-Pro-4.local>
2023-02-07 18:46:11 -0800

    added api stuff, dependency issue

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Home.css';
+import EventsCalendar from '../events/calendar.js';
 
 function Home() {
-  return <div className="bg">Home</div>;
+  return (
+  <>
+  <div className="bg">Home</div>
+  {/* <div>
+      <EventsCalendar />
+    </div> */}
+    </>
+  );
 }
 
 export default Home;
