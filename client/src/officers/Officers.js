15ecf0c8dd5cc1d6127a6d7306bc3aa8eae587bf
Sophia Shen <sophiashen@Sophias-MacBook-Pro-4.local>
2023-04-04 15:22:20 -0700

    morestuff

--- client/src/officers/Officers.js
+++ client/src/officers/Officers.js
@@ -7,7 +7,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+
 
 
 function createData(position, email) {
