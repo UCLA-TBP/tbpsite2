8df4965fab6f55069c94eae823c10201d244862c
ethantjackson <ethantjackson44@gmail.com>
2023-04-10 16:28:30 -0700

    revert setupProxy to development config

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
-      // target: 'http://localhost:8000',
-      target: 'https://tbpsite.onrender.com',
+      target: 'http://localhost:8000',
+      // target: 'https://tbpsite.onrender.com',
       changeOrigin: true,
       pathRewrite: { '^/api': '/' },
     })
