e44730a9fea7e4603dc0c2a58897348a63e58cec
Ethan Jackson <ethanjackson@Ethans-MacBook-Pro.local>
2023-10-16 17:25:21 -0700

    add update functionality to candidate spreadsheet

--- client/src/admin/CandidateSpreadsheet.js
+++ client/src/admin/CandidateSpreadsheet.js
@@ -15,7 +15,7 @@ import {
   Button,
 } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
-import { Container } from '@mui/material';
+import { Container, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import TBPBackground from '../components/TBPBackground';
 import _ from 'lodash';
 
@@ -23,6 +23,107 @@ function CandidateSpreadsheet() {
   const [candidates, setCandidates] = useState([]);
   const [displayedCandidates, setDisplayedCandidates] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [selectedCells, setSelectedCells] = useState(new Set());
+  const [startCellId, setStartCellId] = useState(null);
+  const [mode, setMode] = useState('add');
+
+  const requirements = [
+    'generalSocial1',
+    'generalSocial2',
+    'tutoring',
+    'testBank',
+    'corporate',
+    'interview',
+    'candidateQuiz',
+    'bentPolishing',
+    'initiation',
+    'membershipFee',
+    'newMemberForm',
+    'coffeeChat',
+    'academicOutreach',
+    'socialMediaPost',
+    'chalking',
+  ];
+
+  const disableHighlight = (e) => {
+    e.preventDefault();
+  };
+
+  const handleMouseDown = (candidateIdx, idx) => {
+    setStartCellId(JSON.stringify([candidateIdx, idx]));
+    document.addEventListener('selectstart', disableHighlight);
+  };
+
+  const handleMouseUp = (candidateIdx, idx) => {
+    const startCandidateId = JSON.parse(startCellId)[0];
+    const startReqIdx = JSON.parse(startCellId)[1];
+    const newSelected = new Set(selectedCells);
+    const isFilling = !selectedCells.has(startCellId);
+    for (
+      let i = Math.min(startCandidateId, candidateIdx);
+      i <= Math.max(startCandidateId, candidateIdx);
+      i += 1
+    ) {
+      for (
+        let j = Math.min(startReqIdx, idx);
+        j <= Math.max(startReqIdx, idx);
+        j += 1
+      ) {
+        const cellID = JSON.stringify([i, j]);
+        if (isFilling) {
+          newSelected.add(cellID);
+        } else {
+          newSelected.delete(cellID);
+        }
+      }
+    }
+    setSelectedCells(newSelected);
+  };
+
+  const handleUpdatePayload = () => {
+    const candidateIdxToReqPayload = {};
+    selectedCells.forEach((selected) => {
+      const cellPos = JSON.parse(selected);
+      if (!candidateIdxToReqPayload[cellPos[0]]) {
+        candidateIdxToReqPayload[cellPos[0]] = [];
+      }
+      candidateIdxToReqPayload[cellPos[0]].push(requirements[cellPos[1]]);
+    });
+
+    const updates = [];
+    for (let [candidateIdx, reqPayload] of Object.entries(
+      candidateIdxToReqPayload
+    )) {
+      const candidate = displayedCandidates[candidateIdx];
+      const newRequirements = candidate.requirements;
+      reqPayload.forEach((req) => {
+        newRequirements[req] = mode === 'add';
+      });
+      console.log(candidate, newRequirements);
+      updates.push(
+        axios.put('/api/user/update-user/' + candidate._id, candidate)
+      );
+    }
+    Promise.all(updates)
+      .then((responses) => {
+        const updatedDisplayedCandidates = displayedCandidates.map(
+          (candidate, candidateIdx) => {
+            if (!candidateIdxToReqPayload[candidateIdx]) {
+              return candidate;
+            }
+            const reqPayload = candidateIdxToReqPayload[candidateIdx];
+            reqPayload.forEach((req) => {
+              candidate[req] = mode === 'add';
+            });
+            return candidate;
+          }
+        );
+        setDisplayedCandidates(updatedDisplayedCandidates);
+      })
+      .catch((err) => {
+        window.alert('Could not commit changes.');
+      });
+  };
 
   const HeaderCell = styled(TableCell)(({ theme }) => ({
     padding: '10px',
@@ -69,12 +170,12 @@ function CandidateSpreadsheet() {
           sx={{
             backgroundColor: (theme) => alpha(theme.palette.custom.main, 0.95),
             borderRadius: '12px',
-            padding: '24px 32px 32px',
+            padding: '12px 24px 12px',
           }}
-          mb={3}
+          mb={2}
         >
           <Typography
-            variant='h2'
+            variant='h3'
             color='primary'
             sx={{
               fontWeight: 'bold',
@@ -83,7 +184,12 @@ function CandidateSpreadsheet() {
           >
             Candidate Spreadsheet
           </Typography>
-          <Typography variant='p' color='custom2' mb={1}>
+          <Typography
+            variant='p'
+            color='custom2'
+            mb={1}
+            sx={{ fontSize: '1rem' }}
+          >
             Filter by email, name, major, or initiation quarter (e.g. "spring
             2023")
           </Typography>
@@ -107,6 +213,7 @@ function CandidateSpreadsheet() {
               marginRight: '12px',
             }}
             onClick={() => {
+              setSelectedCells(new Set());
               const query = document
                 .getElementById('query-input')
                 .value.toLowerCase();
@@ -133,12 +240,6 @@ function CandidateSpreadsheet() {
                   if (queryInitiationYear) {
                     queryInitiationYear = parseInt(queryInitiationYear[0]);
                   }
-                  console.log('compare');
-                  console.log(queryInitiationQuarter, queryInitiationYear);
-                  console.log(
-                    candidate.initiationQuarter.quarter,
-                    candidate.initiationQuarter.year
-                  );
                   if (
                     candidate.initiationQuarter.quarter ===
                       queryInitiationQuarter &&
@@ -153,6 +254,46 @@ function CandidateSpreadsheet() {
             }}
           >
             Search
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'right',
+            paddingBottom: '0.2rem',
+            gap: '0.6rem',
+          }}
+        >
+          <ToggleButtonGroup
+            color='secondary'
+            size={'small'}
+            onChange={(e, newMode) => {
+              setMode(newMode);
+            }}
+            value={mode}
+            exclusive
+            sx={{
+              height: '30px',
+              backgroundColor: (theme) =>
+                alpha(theme.palette.custom.main, 0.65),
+            }}
+          >
+            <ToggleButton value='add'>Add</ToggleButton>
+            <ToggleButton value='del'>Del</ToggleButton>
+          </ToggleButtonGroup>
+          <Button
+            color='secondary'
+            variant='contained'
+            sx={{
+              width: '200px',
+              height: '30px',
+              marginBottom: '4px',
+              marginRight: '12px',
+            }}
+            onClick={handleUpdatePayload}
+          >
+            <CheckIcon sx={{ mr: 1 }} />
+            Commit Changes
           </Button>
         </Box>
         <TableContainer
@@ -180,27 +321,15 @@ function CandidateSpreadsheet() {
                 <HeaderCell>Email</HeaderCell>
                 <HeaderCell>Major</HeaderCell>
                 <HeaderCell>Initiation Quarter</HeaderCell>
-                <HeaderCell>General Social 1</HeaderCell>
-                <HeaderCell>General Social 2</HeaderCell>
-                <HeaderCell>Tutoring</HeaderCell>
-                <HeaderCell>Test Bank</HeaderCell>
-                <HeaderCell>Corporate</HeaderCell>
-                <HeaderCell>Interview</HeaderCell>
-                <HeaderCell>Candidate Quiz</HeaderCell>
-                <HeaderCell>Bent Polishing</HeaderCell>
-                <HeaderCell>Initiation</HeaderCell>
-                <HeaderCell>Membership Fee</HeaderCell>
-                <HeaderCell>New Member Form</HeaderCell>
-                <HeaderCell>Coffee Chat</HeaderCell>
-                <HeaderCell>Academic Outreach</HeaderCell>
-                <HeaderCell>Social Media Post</HeaderCell>
-                <HeaderCell>Chalking</HeaderCell>
+                {requirements.map((requirement) => (
+                  <HeaderCell>{_.startCase(requirement)}</HeaderCell>
+                ))}
               </TableRow>
             </TableHead>
             <TableBody>
-              {displayedCandidates.map((candidate) => (
+              {displayedCandidates.map((candidate, candidateIdx) => (
                 <TableRow
-                  key={candidate.email}
+                  key={candidate._id}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                 >
                   <Cell
@@ -224,10 +353,28 @@ function CandidateSpreadsheet() {
                       ' ' +
                       candidate.initiationQuarter?.year}
                   </Cell>
-                  {Object.keys(candidate.requirements).map(
+                  {requirements.map(
                     (requirement, idx) =>
                       idx < 15 && (
-                        <Cell key={requirement}>
+                        <Cell
+                          key={candidate._id + requirement}
+                          onMouseDown={() => {
+                            handleMouseDown(candidateIdx, idx);
+                          }}
+                          onMouseUp={() => {
+                            handleMouseUp(candidateIdx, idx);
+                          }}
+                          sx={{
+                            backgroundColor: selectedCells.has(
+                              JSON.stringify([candidateIdx, idx])
+                            )
+                              ? mode === 'add'
+                                ? 'lightgreen'
+                                : 'pink'
+                              : 'white',
+                            cursor: 'pointer',
+                          }}
+                        >
                           {candidate.requirements[requirement] ? (
                             <CheckIcon color='secondary' />
                           ) : (
