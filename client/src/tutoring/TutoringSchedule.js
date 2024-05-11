6afe027a4bcc349a414d4e48a37c0e35d9252f63
Dillon Broderick <dillonbroderick@g.ucla.edu>
2024-05-11 12:26:05 -0700

    Removed unused vars

--- client/src/tutoring/TutoringSchedule.js
+++ client/src/tutoring/TutoringSchedule.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { Container } from '@mui/system';
 
 function TutoringSchedule() {
