531e63b99263a241a21ab2dfa902022b9d776245
NotAmaia <012.sva@gmail.com>
2026-02-17 21:04:00 -0800

    fixed build failure

--- client/src/components/FacultyList.js
+++ client/src/components/FacultyList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Typography, Box } from "@mui/material";
 import { Container } from "@mui/material";
 import styled from "@emotion/styled";
